d79c7f074f485ded2990e69db69bf4824b0dc1bf
dtklumpp <dtklumpp@gmail.com>
2020-11-01 18:13:48 -0500

    loaded more templates

--- src/pages/Treact.js
+++ src/pages/Treact.js
@@ -3,7 +3,12 @@ import "style.css";
 import "tailwindcss/dist/base.css";
 import AnimationRevealPage from "helpers/AnimationRevealPage";
 import Hero from "components/hero/FullWidthWithImage";
-import Footer from "../components/footers/FiveColumnDark";
+import Footer from "components/footers/FiveColumnDark";
+import SimpleSubscribe from 'components/forms/SimpleSubscribeNewsletter';
+import GridFeatured from 'components/blogs/GridWithFeaturedPost';
+import TabCardGrid from 'components/cards/TabCardGrid';
+import ThreeColumn from 'components/features/ThreeColSimple';
+import BetterHero from 'components/hero/BackgroundAsImageWithCenteredContent';
 
 export default TreactPage;
 
@@ -24,31 +29,31 @@ function TreactPage() {
 <hr/>
 <br/>
 
-
-
-<br/>
-<hr/>
-<br/>
-
-
+        <SimpleSubscribe/>
 
 <br/>
 <hr/>
 <br/>
 
-
-
-<br/>
-<hr/>
-<br/>
-
-
+        <GridFeatured/>
 
 <br/>
 <hr/>
 <br/>
 
+        <TabCardGrid/>
 
+<br/>
+<hr/>
+<br/>
+
+        <ThreeColumn/>
+
+<br/>
+<hr/>
+<br/>
+
+        <BetterHero/>
 
 <br/>
 <hr/>
