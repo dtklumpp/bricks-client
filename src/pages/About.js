f9b15ace51df3ca7d6dc4c0b468e96c4f92f294e
dtklumpp <dtklumpp@gmail.com>
2020-11-01 01:12:55 -0400

    installed antd and got carousel working

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,143 @@
+// import "../holding/slick-carousel/slick/slick.css";
+// import "../holding/slick-carousel/slick/slick-theme.css";
+
+import 'antd/dist/antd.css';
+
+// import './About.css';
+
+import SimpleSlider from '../components/testing/Carousel';
+import SimpleSlider2 from '../components/testing/SimpleSlider';
+
+import { DatePicker } from 'antd';
+import { Carousel } from 'antd';
+
+import {Container} from 'semantic-ui-react';
+import {Grid} from "semantic-ui-react";
+
+
 export default About;
 
+
+function onChange(a, b, c) {
+    console.log(a, b, c);
+  }
+  
+  const contentStyle = {
+    height: '160px',
+    color: '#fff',
+    lineHeight: '160px',
+    textAlign: 'center',
+    background: '#364d79',
+  };
+
 function About() {
-    return <h3>About Page here</h3>
+    return (<>
+        <h3>About Page here</h3>
+        <SimpleSlider/>
+        {/* <SimpleSlider2/> */}
+
+        <br/>
+        <hr/>
+        <br/>
+
+        <DatePicker />
+
+        <br/>
+        <hr/>
+        <br/>
+
+<br/>
+<hr/>
+<br/>
+
+<Container>
+
+    <Grid>
+        <Grid.Column width={9}>
+
+
+  <Carousel afterChange={onChange}>
+    <div>
+      <h3 style={contentStyle}>1</h3>
+    </div>
+    <div>
+      <h3 style={contentStyle}>2</h3>
+    </div>
+    <div>
+      <h3 style={contentStyle}>3</h3>
+    </div>
+    <div>
+      <h3 style={contentStyle}>4</h3>
+    </div>
+  </Carousel>
+        </Grid.Column>
+
+    </Grid>
+<br/>
+<hr/>
+<br/>
+
+    <Carousel effect="fade">
+    <div>
+      <h3 style={contentStyle}>1</h3>
+    </div>
+    <div>
+      <h3 style={contentStyle}>2</h3>
+    </div>
+    <div>
+      <h3 style={contentStyle}>3</h3>
+    </div>
+    <div>
+      <h3 style={contentStyle}>4</h3>
+    </div>
+  </Carousel>,
+
+
+
+</Container>
+
+
+<br/>
+<hr/>
+<br/>
+
+
+
+<br/>
+<hr/>
+<br/>
+
+
+
+<br/>
+<hr/>
+<br/>
+
+
+
+<br/>
+<hr/>
+<br/>
+
+
+
+<br/>
+<hr/>
+<br/>
+
+
+
+<br/>
+<hr/>
+<br/>
+
+
+
+<br/>
+<hr/>
+<br/>
+
+
+
+    </>)
 }
