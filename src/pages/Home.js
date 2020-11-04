cef2ddaaddf90f44f2b7d59fc016f7f21757f4b1
dtklumpp <dtklumpp@gmail.com>
2020-11-04 01:23:32 -0500

    drafted splash page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,19 @@
 import {useState} from 'react';
 import {useEffect} from 'react';
 
+import "style.css";
+import "tailwindcss/dist/base.css";
+
 import '../App.css';
+
+import BetterHero from 'components/hero/BackgroundAsImageWithCenteredContent';
+import ThreeColumn from 'components/features/ThreeColSimple';
+import GridFeatured from 'components/blogs/GridWithFeaturedPost';
+import Footer from "components/footers/FiveColumnDark";
+import SimpleSubscribe from 'components/forms/SimpleSubscribeNewsletter';
+
+
+
 
 export default Home;
 
@@ -11,6 +23,45 @@ function Home() {
         <>
             <h3 class="dtk">Home Page here</h3>
             <h3>unformat-O!</h3>
+
+            <br/>
+            <hr/>
+            <br/>
+
+                    <BetterHero/>
+
+            <br/>
+            <hr/>
+            <br/>
+
+            <ThreeColumn/>
+
+
+            <br/>
+            <hr/>
+            <br/>
+
+            <GridFeatured/>
+
+            <br/>
+            <hr/>
+            <br/>
+
+            <SimpleSubscribe/>
+
+
+            <br/>
+            <hr/>
+            <br/>
+
+            <Footer/>
+
+
+            <br/>
+            <hr/>
+            <br/>
+
+
         </>
     )
 }
--- src/pages/Treact.js
+++ src/pages/Treact.js
@@ -1,6 +1,8 @@
 import React from "react";
+
 import "style.css";
 import "tailwindcss/dist/base.css";
+
 import AnimationRevealPage from "helpers/AnimationRevealPage";
 import Hero from "components/hero/FullWidthWithImage";
 import Footer from "components/footers/FiveColumnDark";
